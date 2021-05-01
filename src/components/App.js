7da14cac65d225baaabd584aaa6868ca4ec21f30
rkboahene <richmond.boahene@akaditi.com>
2021-05-01 18:53:41 +0000

    fix video play issues

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,20 @@ import youtube from "../apis/youtube";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  //   this is the default search term on component mount
+  //   componentDidMount() {
+  //     this.onSearchSubmit("vue");
+  //   }
+
   onSearchSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: { q: term },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -22,11 +31,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onSearchSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="ten wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="six wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
